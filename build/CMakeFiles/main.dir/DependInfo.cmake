7b434dd17ffd62ff13a20be2a40f38837edbd8b3
tcwhite9162 <tcwhite9162@gmail.com>
2026-01-06 20:11:26 +1300

    added build system

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tcwhite/Projects/C-C++/Chess/src/bitboard/bitboard.cpp" "CMakeFiles/main.dir/src/bitboard/bitboard.cpp.o" "gcc" "CMakeFiles/main.dir/src/bitboard/bitboard.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/board.cpp" "CMakeFiles/main.dir/src/board.cpp.o" "gcc" "CMakeFiles/main.dir/src/board.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/eval-helpers.cpp" "CMakeFiles/main.dir/src/eval-helpers.cpp.o" "gcc" "CMakeFiles/main.dir/src/eval-helpers.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/evaluate.cpp" "CMakeFiles/main.dir/src/evaluate.cpp.o" "gcc" "CMakeFiles/main.dir/src/evaluate.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/interface/interface.cpp" "CMakeFiles/main.dir/src/interface/interface.cpp.o" "gcc" "CMakeFiles/main.dir/src/interface/interface.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/moveGen.cpp" "CMakeFiles/main.dir/src/moveGen.cpp.o" "gcc" "CMakeFiles/main.dir/src/moveGen.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/search.cpp" "CMakeFiles/main.dir/src/search.cpp.o" "gcc" "CMakeFiles/main.dir/src/search.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/src/zobrist.cpp" "CMakeFiles/main.dir/src/zobrist.cpp.o" "gcc" "CMakeFiles/main.dir/src/zobrist.cpp.o.d"
+  "/home/tcwhite/Projects/C-C++/Chess/tests/tests.cpp" "CMakeFiles/main.dir/tests/tests.cpp.o" "gcc" "CMakeFiles/main.dir/tests/tests.cpp.o.d"
+  "" "main" "gcc" "CMakeFiles/main.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
